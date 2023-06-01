7c900cec4537567c487ec156f86c0d49b701c2af
Digilaser <134451451+Digilaser@users.noreply.github.com>
2023-06-01 19:17:17 -0300

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,56 @@
+// Para agregar más canciones, simplemente copie el siguiente código y péguelo dentro de la matriz
+
+// {
+// nombre: "Aquí está el nombre de la música",
+// artista: "Aquí está el nombre del artista",
+// img: "nombre de la imagen aquí - recuerde que img debe estar en formato .jpg y está dentro de la carpeta de imágenes de esta carpeta de proyecto",
+// src: "nombre de la música aquí - recuerda que img debe estar en formato .mp3 y está dentro de la carpeta de canciones de esta carpeta de proyecto"
+// }
+
+// péguelo dentro de la matriz tanto como quiera música, entonces no necesita hacer nada más
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  // Me gusta, pégalo y recuerda poner una coma después de terminar este corchete }
+   // {
+   // nombre: "Aquí está el nombre de la música",
+   // artista: "Aquí está el nombre del artista",
+   // img: "nombre de la imagen aquí - recuerde que img debe estar en formato .jpg y está dentro de la carpeta de imágenes de esta carpeta de proyecto",
+   // src: "nombre de la música aquí - recuerda que img debe estar en formato .mp3 y está dentro de la carpeta de canciones de esta carpeta de proyecto"
+   // }
+];
